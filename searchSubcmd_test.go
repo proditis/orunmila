1e051287e42e8f2ddd68d9f3c18dd995ed52ff9f
Pantelis Roditis <proditis@echothrust.com>
2022-12-09 00:45:19 +0200

    split tests to mention the case we're on

--- searchSubcmd_test.go
+++ searchSubcmd_test.go
@@ -8,15 +8,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSearchSubcmd(t *testing.T) {
+// Test undefined argument
+func TestSearchSubcmdUndefinedArgument(t *testing.T) {
 	var buf bytes.Buffer
 	flag.CommandLine.SetOutput(&buf)
-	args := []string{"-defined"}
+	args := []string{"-undefined"}
 
 	err := searchSubcmd(args)
-	assert.EqualError(t, err, `flag provided but not defined: -defined`)
+	assert.EqualError(t, err, `flag provided but not defined: -undefined`)
 
-	args = []string{"-help"}
-	err = searchSubcmd(args)
+}
+
+// test help argument
+func TestSearchSubcmdHelp(t *testing.T) {
+	var buf bytes.Buffer
+	flag.CommandLine.SetOutput(&buf)
+	args := []string{"-help"}
+	err := searchSubcmd(args)
 	assert.EqualError(t, err, `flag: help requested`)
+}
+
+// test tags
+func TestSearchSubcmdTags(t *testing.T) {
+	var buf bytes.Buffer
+	flag.CommandLine.SetOutput(&buf)
+	args := []string{"-tags", "a,b,c"}
+	err := searchSubcmd(args)
+	t.Error(err)
 }
