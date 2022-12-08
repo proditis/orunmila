d83dd205b58b29656834ef8d7be8f4fc2e2d698e
Pantelis Roditis <proditis@echothrust.com>
2022-12-08 13:49:15 +0200

    implement some more test cases pass

--- helpers_test.go
+++ helpers_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
+func init() {
+}
 func TestGetDefaultDBPath(t *testing.T) {
 	path, _ := os.Getwd()
 	msg := getDefaultDBPath()
@@ -31,35 +35,153 @@ func TestIsFileExists(t *testing.T) {
 }
 
 func TestTagsToString(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
+	Tags = make(map[string]int64)
+
+	wants := ""
+	if TagsToString() != wants {
+		t.Fatal(`Error: TagsToString not empty`)
+	}
+	Tags["test"] = 1
+	wants = "1"
+	got := TagsToString()
+	if got != wants {
+		t.Fatalf(`Error: TagsToString %s not %s`, got, wants)
+	}
+	Tags["test2"] = -1
+	wants = "1,-1"
+	if TagsToString() != wants {
+		t.Fatalf(`Error: TagsToString not %s`, wants)
+	}
+
 }
 
 func TestGetTagId(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
+	// requires db access
+	Tags = make(map[string]int64)
+	t.Fatalf(`Error: not implemented %v`, Tags)
 }
 func TestGetWordId(t *testing.T) {
+	// requires db access
 	t.Fatal(`Error: not implemented`)
 }
 
 func TestCreateDB(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
+	// cleanup any existing files
+	os.Remove("random.db")
+
+	// create database
+	err := createDB("random.db")
+	if err != nil {
+		t.Fatalf(`Error: createDB failed %v`, err)
+	}
+
+	err = createDB("random.db")
+	if err != nil {
+		t.Fatalf(`Error: 2nd createDB failed, %v`, err)
+	}
+	defer os.Remove("random.db")
 }
 
 func TestStringToArray(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
+	var wants map[string]int64
+	wants = map[string]int64{
+		"a": -1,
+		"b": -1,
+		"c": -1,
+	}
+	got := stringToArray("a,b,c")
+	if !reflect.DeepEqual(got, wants) {
+		t.Fatalf(`Error: stringToArray wants %v got %v`, wants, got)
+	}
+
+	got = stringToArray("a,b")
+	if reflect.DeepEqual(got, wants) {
+		t.Fatalf(`Error: stringToArray wants %v got %v`, wants, got)
+	}
+
+	got = stringToArray("")
+	wants = map[string]int64{}
+	if !reflect.DeepEqual(got, wants) {
+		t.Fatalf(`Error: stringToArray wants %v got %v`, wants, got)
+	}
 }
 
 func TestPopulateTagIds(t *testing.T) {
 	t.Fatal(`Error: not implemented`)
 }
-func Test(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
-}
 func TestRemoveEmptyTags(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
+	var wants map[string]int64
+	wants = map[string]int64{}
+	Tags = map[string]int64{
+		"a": -1,
+		"b": -1,
+		"c": -1,
+	}
+	removeEmptyTags()
+	if !reflect.DeepEqual(Tags, wants) {
+		t.Fatalf(`Error: removeEmptyTags want %v got %v`, wants, Tags)
+	}
+
+	wants = map[string]int64{"b": 1}
+	Tags = map[string]int64{
+		"a": -1,
+		"b": 1,
+		"c": -1,
+	}
+	removeEmptyTags()
+	if !reflect.DeepEqual(Tags, wants) {
+		t.Fatalf(`Error: removeEmptyTags want %v got %v`, wants, Tags)
+	}
 }
 func TestImportTags(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
+	var wants map[string]int64
+	Tags = make(map[string]int64)
+	Tags = map[string]int64{
+		"a": -1,
+		"b": -1,
+		"c": -1,
+	}
+	wants = map[string]int64{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+	createDbFileifNotExists("random.db")
+
+	db, err := sql.Open("sqlite3", "random.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+	defer os.Remove("random.db")
+
+	importTags(db)
+	if !reflect.DeepEqual(Tags, wants) {
+		t.Fatalf(`Error: importTags wants %v got %v`, wants, Tags)
+	}
+
+	Tags = map[string]int64{
+		"a": -1,
+		"b": -1,
+		"c": -1,
+	}
+	importTags(db)
+	if !reflect.DeepEqual(Tags, wants) {
+		t.Fatalf(`Error: importTags didnt populate properly wants %v got %v`, wants, Tags)
+	}
+
+	Tags = map[string]int64{
+		"a": -1,
+		"b": -1,
+		"c": -1,
+		"d": -1,
+	}
+	wants["d"] = 4
+	importTags(db)
+	if !reflect.DeepEqual(Tags, wants) {
+		t.Fatalf(`Error: importTags didnt populate properly with new entry wants %v got %v`, wants, Tags)
+	}
+
 }
 func TestImportFileWords(t *testing.T) {
 	t.Fatal(`Error: not implemented`)
@@ -67,6 +189,18 @@ func TestImportFileWords(t *testing.T) {
 func TestSearchWordsByTagIds(t *testing.T) {
 	t.Fatal(`Error: not implemented`)
 }
+
 func TestCreateDbFileifNotExists(t *testing.T) {
-	t.Fatal(`Error: not implemented`)
+	var dbname = "random.db"
+	_, err := os.Stat(dbname)
+	if err != nil && !os.IsNotExist(err) {
+		t.Fatal(err)
+	}
+	createDbFileifNotExists(dbname)
+	defer os.Remove(dbname)
+
+	_, err = os.Stat(dbname)
+	if os.IsNotExist(err) {
+		t.Fatal(err)
+	}
 }
