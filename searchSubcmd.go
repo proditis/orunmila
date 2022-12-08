e180c3fa50dd099157df1d13e383cdabc5d59dd4
Pantelis Roditis <proditis@echothrust.com>
2022-12-08 23:20:34 +0200

    added usage and ShouldReturn flag Changed ExitOnError into ContinueOnError Check parse for err and return if not nil

--- searchSubcmd.go
+++ searchSubcmd.go
@@ -9,15 +9,28 @@ import (
 )
 
 // parse args of the search subcommand and exec it
-func searchSubcmd(args []string) {
-	searchCmd := flag.NewFlagSet("search", flag.ExitOnError)
+func searchSubcmd(args []string) error {
+	searchCmd := flag.NewFlagSet("search", flag.ContinueOnError)
+	ShouldReturn := false
+	searchCmd.Usage = func() {
+		searchCmd.SetOutput(flag.CommandLine.Output())
+		fmt.Fprint(searchCmd.Output(), "Display words matching an optional list of tags\n\n")
+		fmt.Fprintf(searchCmd.Output(), "Usage of orunmila search:\n")
+		fmt.Fprintf(searchCmd.Output(), "orunmila [-db <db_path>] [-debug] search [-tags OPTIONAL_TAGS]\n\n")
+		searchCmd.PrintDefaults()
+		ShouldReturn = true
+	}
 
 	var (
 		tagsPtr = searchCmd.String("tags", "", "a comma separated list of the tags to use")
 	)
 
-	searchCmd.Parse(args)
+	err := searchCmd.Parse(args)
 
+	if ShouldReturn {
+		log.Println(err)
+		return err
+	}
 	log.Debugln("[searchSubcmd] using db:", *dbPtr)
 	log.Debugln("[searchSubcmd] using tags:", *tagsPtr)
 	log.Println("[searchSubcmd] performing a search")
@@ -30,4 +43,5 @@ func searchSubcmd(args []string) {
 	defer db.Close()
 
 	searchWordsByTagIds(db, *tagsPtr)
+	return nil
 }
